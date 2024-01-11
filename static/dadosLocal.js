a55f5572af955dedfd93e44e3dddcabd82a5a474
Patrikmagalhaes <patrikmagalhaes@icloud.com>
2024-01-10 23:31:18 -0300

    criando elementos com js

--- static/dadosLocal.js
+++ static/dadosLocal.js
@@ -1,19 +1,33 @@
 let arrayObejetos = []
-document.addEventListener("DOMContentLoaded", () => {
+let listElement = document.getElementById('lista')
 
+
+document.addEventListener("DOMContentLoaded", () => {
    for (let chave of Object.keys(localStorage)) {
-      valor = JSON.parse(localStorage.getItem(chave))
+      let valor = JSON.parse(localStorage.getItem(chave))
       arrayObejetos.push(valor)
    }
 
    for (i = 0; i < arrayObejetos.length; i++) {
-      console.log('teste', i,arrayObejetos[i]  )
-
+      exibirLista(arrayObejetos, i)
    }
+   
+   function exibirLista(objetos, indiceObjeto) {
 
-   function exibirLista(items) {
-    console.log(items)
+      let ulElement = document.createElement('ul')
+      listElement.appendChild(ulElement)
+      const liElement = document.createElement('li')
+
+      const diaSemana = objetos[indiceObjeto]["diaSemana"]
+      const hora = objetos[indiceObjeto]["hora"]
+      const caminhoPasta = objetos[indiceObjeto]["caminhoPasta"]
+      const impressora = objetos[indiceObjeto]["impressora"]
+
+      ulElement.appendChild(liElement)
+
+      liElement.textContent = diaSemana
+      liElement.textContent = diaSemana
+    
    }
-
 })
 
